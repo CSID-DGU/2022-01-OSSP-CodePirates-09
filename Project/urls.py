eed1e919795ddc77055c01d45a20286139721eb8
Kim-TaeUk <xodnr0130@naver.com>
2022-05-24 19:50:49 +0900

    Fix - url mapping 수정

--- Project/urls.py
+++ Project/urls.py
@@ -1,27 +1,17 @@
+from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from .views import Main, Default
 
-
-# serializer.py로 따로 빼기
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-
-#
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-#
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-#
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+
+    path('', Main.as_view()),
+    path('default', Default.as_view()),
+
+    path('User/', include('User.urls')),
+    path('Course/', include('Course.urls')),
+
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- User/views.py
+++ User/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import UserInfo, UserPreference, UserPartner
+from .serializers import UserInfoSerializer, UserPreferenceSerializer, UserPartnerSerializer
+from rest_framework.parsers import JSONParser
 
 
-class SignUp(APIView):
+@csrf_exempt
+class SignUp():
+    def post(self, request):
+        pass
+
+
+class SignIn():
     def get(self, request):
-        return render(request, "User/signup.html")
+        pass
 
+<<<<<<< HEAD
+# class SignUp(APIView):
+#     def get(self, request):
+#         return render(request, "User/signup.html")
+#
+#     def post(self, request):
+#         # 구현
+#         return render(request, "User/signup.html")
+#
+#
+# class SignIn(APIView):
+#     def get(self, request):
+#         return render(request, "User/signin.html")
+#
+#     def post(self, request):
+#         # 구현
+#         return render(request, "User/signin.html")
+=======
     def post(self, request):
         # 구현
-        return render(request, "User/signup.html")
-
-
-class SignIn(APIView):
-    def get(self, request):
         return render(request, "User/signin.html")
-
-    def post(self, request):
-        # 구현
-        return render(request, "User/signin.html")
+>>>>>>> 304fb26aeb2e0ab653765633567c12ccaf8b80a2
