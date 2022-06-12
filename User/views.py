ea80a18c5f70dde42bd5b182ea02aa2a101331ce
changhyun9 <ckdgus1713@naver.com>
2022-06-12 11:51:44 +0900

    Feat - 추천 코스 기능추가

--- User/views.py
+++ User/views.py
@@ -11,12 +11,15 @@ from django.shortcuts import render, redirect
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from openpyxl import load_workbook
 
 import csv
 import time
 import re  # 정규 표현식을 지원
 import json
 import bcrypt  # 비밀번호 암호화를 위해서 패키지 다운로드
+import pandas as pd # 크롤링하고 분석끝난 데이터 엑셀 파일을 가져오기 위한 import
+import openpyxl # 엑셀 파일을 열고 데이터를 읽기
 
 
 @csrf_exempt
@@ -72,7 +75,7 @@ def signup(request):
             "userSex": request.POST['userSex'],
             "userAge": request.POST['userAge']
         }
-        print(data)
+
         # data = JSONParser().parse(request)  # 넘어온 requset들을 Json 형태로 변환
 
         for key, val in data.items():  # json은 key-value 형태 이기 때문에 key,val값으로 for문 진행
@@ -100,7 +103,7 @@ def signup(request):
             "userSex": data.get("userSex"),
             "userAge": data.get("userAge")
         }
-        print(userinfo)
+
         userinfo_serializer = UserInfoSerializer(data=userinfo)  # userinfo로 저장한 딕셔너리형태 db에 저장
         if userinfo_serializer.is_valid():
             userinfo_serializer.save()
@@ -112,11 +115,11 @@ def signup(request):
                 "userPreferenceDrink": data.get("userPreferenceDrink"),
                 "userPreferenceCafe": data.get("userPreferenceCafe")
             }
-            print(preference)
+
             prefer_serializer = UserPreferenceSerializer(data=preference)  # preference로 저장한 딕셔너리형태 db에 저장
             if prefer_serializer.is_valid():
                 prefer_serializer.save()
-                print("여기서 에러면 실망")
+
                 return render(request, 'Project/main.html', {'message': '회원 가입 성공'}, status=201)  # 두 테이블이 모두 저장되면 회원가입 성공 !
 
         return HttpResponse({"message : Error"}, status=400)  # 그렇지 않으면 실패
@@ -289,11 +292,11 @@ def deleteuser(request):
 @csrf_exempt
 def crawling(request):
     # if request.method == 'GET': # 일단 위치 주소 받아야하고, 카테고리 누른 거받아야 해서 get으로 설정
-    location = "충무로역"  # 나중에는 입력받은 주소로 치환하기 request.get('location')
-    category = "밥집"  # 나중에는 입력받은 카테고리로 치환하기 request.get('categoey')로 변경
-    purpose = "점심식사"  # 나중에는 입력받은 방문목적으로 치환하기
-    preference = UserPreference.objects.get(userId=request.session['user'])  # 세션에 저장된 userId 사용해서 선호도 객체반환
-
+    location = "잠실역"  # 나중에는 입력받은 주소로 치환하기 request.get('location')
+    category = "카페"  # 나중에는 입력받은 카테고리로 치환하기 request.get('categoey')로 변경
+    purpose = ""  # 나중에는 입력받은 방문목적으로 치환하기
+    # preference = UserPreference.objects.get(userId=request.session['user'])  # 세션에 저장된 userId 사용해서 선호도 객체반환
+    mood = ""
     test1 = "preference."
 
     driver = webdriver.Chrome('/Users/munchanghyeon/opt/chromedriver')  # 나중에는 프로젝트 파일에 포함시키기
@@ -307,7 +310,7 @@ def crawling(request):
     search = driver.find_element_by_css_selector('#txt_keyword')  # 검색창 css 선택
     search.click()  # 입력을 위해 검색창 클릭
 
-    search.send_keys('충무로역') # 클릭된 검색창에 특정 단어 입력 //  나중에는 location으로 대체 //
+    search.send_keys(location) # 클릭된 검색창에 특정 단어 입력 //  나중에는 location으로 대체 //
     search.send_keys(Keys.ENTER)  # 입력을 완료하고 엔터키를 누름
 
     # 카테고리, 방문목적, 분위기 다 보기위해서 펼치기 버튼 클릭 첫번째는 카테고리, 두번째는 방문목적, 세번째는 분위기
@@ -320,7 +323,7 @@ def crawling(request):
 
     # if문으로 카테고리 입력에 따라 클릭을 다르게 진행, 몇개 케이스가 없기 때문에 미리 진행
     if category == "술집":
-        driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[3]/ul/li[1]/div').click()
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[3]/ul/li[4]/div').click()
         preferenceCategory = "preferenceDrink"
     elif category == "밥집":
         driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[3]/ul/li[2]/div').click()
@@ -342,20 +345,20 @@ def crawling(request):
 
     driver.implicitly_wait(2)
 
-    # if test1 + preferenceCategory == "가성비좋은":
-    #     driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[1]/div').click()
-    # elif test1 + preferenceCategory == "분위기좋은":
-    #     driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[2]/div').click()
-    # elif test1 + preferenceCategory == "고급스러운":
-    #     driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[5]/div').click()
-    # elif test1 + preferenceCategory == "시끌벅적한":
-    #     driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[7]/div').click()
-    # elif test1 + preferenceCategory == "조용한":
-    #     driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[8]/div').click()
-    # elif test1 + preferenceCategory == "깔끔한":
-    #     driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[9]/div').click()
-    # elif test1 + preferenceCategory == "예쁜":
-    #     driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[12]/div').click()
+    if mood == "가성비좋은":
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[1]/div').click()
+    elif mood == "분위기좋은":
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[2]/div').click()
+    elif mood == "고급스러운":
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[5]/div').click()
+    elif mood == "시끌벅적한":
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[7]/div').click()
+    elif mood == "조용한":
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[8]/div').click()
+    elif mood == "깔끔한":
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[9]/div').click()
+    elif mood == "예쁜":
+        driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div[8]/ul/li[12]/div').click()
 
     # 더보기 클릭해서 넘어가는 페이지 에서 크롤링 진행
     more_btn = driver.find_element_by_css_selector('#root > div > div > div.Body > div.RList > div > button')  # 결과 더보기
@@ -364,16 +367,82 @@ def crawling(request):
     driver.implicitly_wait(2)  # 웹사이트 로딩 될때까지 2초 기다림
 
     items = driver.find_elements_by_css_selector('.PoiBlock')  # 상품 정보를 모두 가지고 있는 div 태그 지정
-    print(len(items))
+
+    # f = open(r"/Users/munchanghyeon/PycharmProjects/2022-01-OSSP1-CodePirates-09/data.csv", 'w', encoding='utf-8')
+    # csvWriter = csv.writer(f)
 
     for item in items:  # for문을 사용해서 상품 이름, 상품 주소를 출력
         name = item.find_element_by_css_selector('.InfoHeader > h2').get_attribute('innerText')  # .text가 안먹어서 설정
         address = item.find_element_by_css_selector('.Category > span').get_attribute('innerText')  # 나중에 카테고리도 출력
-        review = item.find_element_by_css_selector('.Review').get_attribute('innerText')  # 리뷰 출력
+        try:
+            review = item.find_element_by_css_selector('.Review').get_attribute('innerText')  # 리뷰 출력
+        except:
+            review = "리뷰가 없습니다."
         name = re.sub(r"[0-9.]", "", name)
         print("가게명 : " + name)
         print("가게 주소 : " + address)
         print("대표 리뷰 : " + review)
         print("######")
+      #  csvWriter.writerow([name, address, review])
 
+    # f.close()
     return 0  # 나중에는 이동할 페이지나 리턴 값 구현
+
+@csrf_exempt
+def createcourse(request):
+    if request.method == 'GET':
+        # data = {
+        #     "location": request.GET['location'], # 검색한 위치 받기
+        #     "1stcategory": request.GET['1stcategory'], # 첫번째 카테고리 받기
+        #     "2ndcategory": request.GET['2ndcategory'], # 두번째 카테고리 받기
+        #     "3rdcategory": request.GET['3rdcategory'] # 세번쨰 카테고리 빋기
+        # }
+        data = JSONParser().parse(request)
+
+        firstcourse = data.get('location') + "." + data.get('1stcategory') # 첫번째 코스 sheet 저장
+        secondcourse = data.get('location') + "." + data.get('2ndcategory') # 두번째 코스 sheet 저장
+        thirdcourse = data.get('location') + "." + data.get('3rdcategory') # 세번쨰 코스 sheet 저장
+
+        filename = "data.xlsx" # 열기위한 파일명 저장
+        xlxs = load_workbook(filename=filename) # 저장한 filename를 통해서 엑셀 파일 열기
+        # xlxs = pd.read_excel(filename, sheet_name=firstcourse)
+
+        onesheet = xlxs[firstcourse] # 첫번째 카테고리에 대한 시트명 저장
+        twosheet = xlxs[secondcourse] # 두번째 카테고리에 대한 시트명 저장
+        threesheet = xlxs[thirdcourse] # 세번쨰 카테고리에 대한 시트명 저장
+
+        fincourse = [] # 최종 코스를 저장하기 위한 리스트
+
+        row = onesheet[2] # 가져올 열 번호 지정
+        for cell in row:
+            fincourse += [cell.value] # 추출된 데이터를 추가
+
+        row = twosheet[3] # 가져올 열 번호 저장
+        for cell in row:
+            fincourse += [cell.value] # 추출된 데이터를 추가
+
+        row = threesheet[15] # 가져올 열 번호 저장
+        for cell in row:
+            fincourse += [cell.value] # 추출된 데이터를 추가
+
+        print(fincourse)
+        print(onesheet['C1'].value) # 하나의 셀을 저장하는 방식
+
+        return 0
+
+# 내가 해야하는거 뭘까요 ?
+# 일단은 Ui를 구성해야하고 완료 버튼을 눌렀을 때,
+# 검색한 지역과 코스의 카테고리가 넘어와야함
+# 그럼 그것을 바탕으로 랜덤을 코스를 3개를 추천하는 것을 구현
+# 그리고 그걸 프론트에다가 출력해야함
+
+# 프론트에서는 다시 딕셔너리 형태로 넘어오는 가게 데이터를 표시하는 것
+# 그리고 검색하는 입력 인풋을 만들고 폼태그로 감싸서 카테고릴아 같이 넘어갈 수 있게 만들 것 !
+
+
+# 0. 프론트단에서 검색하는 태그 하나랑 완료 버튼 누르면 form 태그로 감싸서 백에 넘어오는 것까지 선행되어야 함
+# 1. 서버단에서 지역, 카테고리 받아서 가게 셋업하는 걸 (토요일까지)
+# 2. 서버에서 넘어온 가게 데이터를 프론트에서 출력하는 건 (일요일)
+# 3. 그리고 남은 페이지 연결과 자잘한 건 월요일 또는 화요일 또는 수요일에 끝내기
+# 4. 수요일 에는 데모 영상이랑 ppt 자료 만들어서 공소 끝내기
+# 5. 그리고 readme 작성하기
